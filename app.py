5eb8b1331e66518157a7c6409dc16126ca37147b
Joshua Scarbrough <159404014+JoshuaScarbrough@users.noreply.github.com>
2024-06-26 10:46:09 -0500

    Update app.py
    Included the stories class so that the page wouldn't be static

--- app.py
+++ app.py
@@ -2,9 +2,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-
 # They gave you a base class which is inside of the stories.py file. You need to import his class into your app.py
-from stories import Story
+from stories import story
 
 
 @app.route('/')
@@ -16,18 +15,18 @@ def base_page():
 def madlibs_start():
     """ Geneate and show form to ask words"""
 
-    
-    return render_template("madlibs_start.html")
+    prompts = story.prompts
+    return render_template("madlibs_start.html", prompts=prompts)
 
 
 @app.route("/madlibs_completion")
 def madlibs_completion():
-    place = request.args["place"]
-    noun = request.args["noun"]
-    verb = request.args["verb"]
-    adjective = request.args["adjective"]
-    plural_noun = request.args["plural_noun"]
-    return render_template("madlibs_completion.html", place=place, noun=noun, verb=verb, adjective=adjective, plural_noun=plural_noun)
+   
+    text = story.generate(request.args)
+    return render_template("madlibs_completion.html", text=text)
+
+
+
 
 
 if __name__ == '__main__':
